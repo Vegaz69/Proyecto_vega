6dffca6aafcaca99d6d8c9a80bb68a833d385b0b
CodinRes <samuelnzini@hotmail.com>
2025-06-29 17:14:13 -0300

    reinstalado vendor con composer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeigniter4/framework',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '3a25fc2cbcede5b123566328f4e53fd4108c794a',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -29,9 +29,9 @@
             'dev_requirement' => true,
         ),
         'codeigniter4/framework' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '3a25fc2cbcede5b123566328f4e53fd4108c794a',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
